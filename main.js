b2c4be8cc0ad3acbc9d08d6aa2c88fe9032508a6
cnqinglin <2207328000@qq.com>
2019-09-08 12:52:14 +0800

    jQuery.ajax 满足 Promise 规则

--- main.js
+++ main.js
@@ -1,29 +1,73 @@
+window.jQuery = function (nodeOrSelector) {
+    var nodes = {}
+    nodes.addClass = function () {}
+    nodes.html = function () {}
+    return nodes
+}
+
+window.jQuery.ajax = function ({ //AJAX必须是返回一个promise对象
+    method,
+    url,
+    body,
+    headers
+}) {//回调：return new promise(function(resolve,reject){})
+    return new Promise(function (resolve, reject) { //promise就是window下面的一个全局属性
+        let request = new XMLHttpRequest()
+        //设置http请求的头信息和数据体	
+        request.open(method, url) //设置http请求的第一部分	
+        for (let key in headers) {
+            let value = headers[key] //拿到value	
+            request.setRequestHeader(key, value)
+        }
+        
+        /*
+        
+        window.Promise = function(fn){
+            （return new promise(function(resolve,reject){})）
+            /...
+            .then(
+                ()=>{},
+                ()=>{}
+            )
+        }
+        */
+
+        //事件监听	
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                //console.log('请求响应完毕')	
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    //console.log('请求失败了')	
+                    reject.call(undefined, request)
+                }
+            }
+        }
+        request.send(body) //设置http请求的第四部分	
+    })
+}
+
+window.$ = window.jQuery //在Stack栈内存中存同一个地址	
+
+function f1(responseText) {}
+function f2(responseText) {}
+
+//使用方代码	
 myButton.addEventListener('click', (e) => {
-
-
-    $.ajax({
+    window.$.ajax({ 	
         url: '/xxx',
         method: 'post',
-        error: (x) => {
-            console.log(x)
-            console.log(x.status)
-            console.log(x.statusText)
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'qinglin': '18'
         }
-    }).then(  //promise规范，不需要串具名变量，标准化操作
-        (responseText) => {
-            console.log(responseText);
-            return responseText  //若成功，则这个return值会传到第二个then的第一个参数并执行
+    }).then(//.then()也会返回一个promise对象，所以可以加多个.then()
+        (text) => {
+            console.log(text)
         },
         (request) => {
-            console.log('error1');
-            return  '已经处理'
-        }).then(  //对一个结果进行多次处理就继续加 .then()
-            (上一次的处理结果) => {
-                console.log(上一次的处理结果)//执行结果是对象，原因：jQuery一看到后台传给的是JSON字符串，会自动转换为对象
-            },
-            (request) => {
-                console.log('error2')
-            }
-            )
-
+            console.log(request)
+        }
+    )
 })
